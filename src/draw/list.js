a4a99b3fefccd12c99fa70c1d8e33ccf8adc4297
wangqy1994 <1300146731@qq.com>
2016-11-30 16:57:15 +0800

    key的图标新做

--- src/draw/list.js
+++ src/draw/list.js
@@ -4,17 +4,5 @@
 "use strict";
 const defaultColor = require("../constant").defaultValue.color;
 module.exports = {
-    "mywallet": [
-        {
-            type: "circle",
-            value: {
-                cx: 11.163,
-                cy: 8.585,
-                r: 0.477
-            },
-            props: {
-                fill: defaultColor
-            }
-        }
-    ]
+   
 };
