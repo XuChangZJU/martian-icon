5c7a1211645d4b2c89778909fcc886ea888341f7
wangqy1994 <1300146731@qq.com>
2016-11-30 15:42:36 +0800

    添加图标新文件

---
+++ src/draw/list.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Administrator on 2016/11/30.
+ */
+"use strict";
+const defaultColor = require("../constant").defaultValue.color;
+module.exports = {
+    "mywallet": [
+        {
+            type: "circle",
+            value: {
+                cx: 11.163,
+                cy: 8.585,
+                r: 0.477
+            },
+            props: {
+                fill: defaultColor
+            }
+        }
+    ]
+};
---
+++ src/path/key.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/11/30.
+ */
+"use strict";
+
+module.exports = {
+    "add1":
+        []
+};
