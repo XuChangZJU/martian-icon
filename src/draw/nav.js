579eb7c7ec8a5aab9ebc82285a3af504d83fdd61
wangqy1994 <1300146731@qq.com>
2017-12-25 16:09:14 +0800

    首页底部功能图标

--- src/draw/nav.js
+++ src/draw/nav.js
@@ -4,5 +4,15 @@
 "use strict";
 const defaultColor = require("../constant").defaultValue.color;
 module.exports = {
-    
+    "honepage": [
+        {
+            type: "points",
+            value: [[5.35, 4.98],[5.35, 5], [5.35, 5]],
+            props: {
+                fill: defaultColor
+            }
+        }
+    ],
+
+
 };
